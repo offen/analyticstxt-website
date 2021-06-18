4707298daf8fc363d3b19a4622fdd95278453d16
Frederik Ring <frederik.ring@gmail.com>
2021-06-18 14:48:28 +0200

    model form markup from data

--- website/theme/static/scripts/wizard.js
+++ website/theme/static/scripts/wizard.js
@@ -1,59 +1,86 @@
-var initialModel = {
-  Author: { values: [] },
-  Collects: { values: [] },
-  Stores: { values: [] }
-}
-
-new window.Vue({ // eslint-disable-line no-new
-  el: '#wizard',
-  delimiters: ['<%', '%>'],
-  data: {
-    model: initialModel,
-    options: {
-      Collects: [
-        { label: 'None', value: 'none' },
-        { label: 'URL', value: 'url' },
-        { label: 'IP Address', value: 'ip-address' },
-        { label: 'Geolocation', value: 'geo-location' },
-        { label: 'User Agent', value: 'user-agent' },
-        { label: 'Fingerprint', value: 'fingerprint' },
-        { label: 'Device Type', value: 'device-type' },
-        { label: 'Referrer', value: 'referrer' },
-        { label: 'Visit Duration', value: 'visit-duration' },
-        { label: 'Custom Events', value: 'custom-events' },
-        { label: 'Session Recording', value: 'session-recording' }
-      ],
-      Stores: [
-        { label: 'None', value: 'none' },
-        { label: 'First Party Cookies', value: 'first-party-cookies' },
-        { label: 'Third Party Cookies', value: 'third-party-cookies' },
-        { label: 'Local Storage', value: 'local-storage' },
-        { label: 'Cache', value: 'cache' }
-      ]
+void (function (Vue, parser) {
+  new Vue({ // eslint-disable-line no-new
+    el: '#wizard',
+    delimiters: ['<%', '%>'],
+    data: {
+      fields: {
+        Author: {
+          isInput: true,
+          value: null,
+          toModel: function (value) {
+            return { values: [value].filter(Boolean) }
+          }
+        },
+        Collects: {
+          isMultiValue: true,
+          value: [],
+          toModel: function (value) {
+            return { values: value }
+          },
+          options: [
+            { label: 'None', value: 'none' },
+            { label: 'URL', value: 'url' },
+            { label: 'IP Address', value: 'ip-address' },
+            { label: 'Geolocation', value: 'geo-location' },
+            { label: 'User Agent', value: 'user-agent' },
+            { label: 'Fingerprint', value: 'fingerprint' },
+            { label: 'Device Type', value: 'device-type' },
+            { label: 'Referrer', value: 'referrer' },
+            { label: 'Visit Duration', value: 'visit-duration' },
+            { label: 'Custom Events', value: 'custom-events' },
+            { label: 'Session Recording', value: 'session-recording' }
+          ]
+        },
+        Stores: {
+          isMultiValue: true,
+          value: [],
+          toModel: function (value) {
+            return { values: value }
+          },
+          options: [
+            { label: 'None', value: 'none' },
+            { label: 'First Party Cookies', value: 'first-party-cookies' },
+            { label: 'Third Party Cookies', value: 'third-party-cookies' },
+            { label: 'Local Storage', value: 'local-storage' },
+            { label: 'Cache', value: 'cache' }
+          ]
+        },
+        Varies: {
+          isSingleValue: true,
+          value: null,
+          toModel: function (value) {
+            return { values: [value].filter(Boolean) }
+          },
+          options: [
+            { label: 'None', value: 'none' },
+            { label: 'Random', value: 'random' },
+            { label: 'Geographic', value: 'geographic' },
+            { label: 'Behavioral', value: 'behavioral' }
+          ]
+        }
+      }
     },
-    fieldAuthor: initialModel.Author.values.join(''),
-    fieldCollects: initialModel.Collects.values,
-    fieldStores: initialModel.Stores.values
-  },
-  computed: {
-    output: function () {
-      var result = window.analyticstxtParser.serialize(this.model, { lax: true })
-      return (result[1] && result[1].message) || result[0]
+    methods: {
+      model: function () {
+        return Object.keys(this.fields).reduce(function (acc, key) {
+          var clone = JSON.parse(JSON.stringify(this.fields[key].value))
+          var asModel = this.fields[key].toModel(clone)
+          if (asModel.values && asModel.values.length) {
+            acc[key] = asModel
+          }
+          return acc
+        }.bind(this), {})
+      }
     },
-    error: function () {
-      var result = window.analyticstxtParser.serialize(this.model)
-      return result[1]
+    computed: {
+      output: function () {
+        var result = parser.serialize(this.model(), { lax: true })
+        return (result[1] && result[1].message) || result[0]
+      },
+      error: function () {
+        var result = parser.serialize(this.model())
+        return result[1]
+      }
     }
-  },
-  watch: {
-    fieldAuthor: function (newValue) {
-      this.model.Author.values = [newValue]
-    },
-    fieldCollects: function (newValue) {
-      this.model.Collects.values = newValue
-    },
-    fieldStores: function (newValue) {
-      this.model.Stores.values = newValue
-    }
-  }
-})
+  })
+})(window.Vue, window.analyticstxtParser)
