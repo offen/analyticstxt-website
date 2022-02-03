867bd14db6b19498e694f32a824a86b626d1c8f8
Frederik Ring <frederik.ring@gmail.com>
2022-02-03 09:58:37 +0100

    Allow decoration of rendered markdown

--- website/theme/static/scripts/explainer.js
+++ website/theme/static/scripts/explainer.js
@@ -3,23 +3,32 @@ void (function (Vue, parser, marked) {
     el: '#explainer',
     delimiters: ['<%=', '%>'],
     data: {
-      value: null
-    },
-    methods: {
-      explain: function (e) {
-        this.value = document.querySelector('#explainer-content').value
+      value: null,
+      output: null,
+      decorateContent: {
+        'p': ['mt4', 'pa1']
       }
     },
-    computed: {
-      output: function () {
-        if (!this.value) {
-          return ''
-        }
-        const [result, error] = parser.explain(this.value)
-        if (error) {
-          return 'The given analytics.txt file is not valid and cannot be used to generate an explanation.'
-        }
-        return marked.parse(result)
+    methods: {
+      explain: function () {
+        this.output = (() => {
+          if (!this.value) {
+            return ''
+          }
+          const [result, error] = parser.explain(this.value)
+          if (error) {
+            return 'The given analytics.txt file is not valid and cannot be used to generate an explanation.'
+          }
+          const html = marked.parse(result)
+          const container = document.createElement('div')
+          container.innerHTML = html
+          for (const [selector, decorator] of Object.entries(this.decorateContent)) {
+            for (const element of container.querySelectorAll(selector)) {
+              element.classList.add.apply(element.classList, decorator)
+            }
+          }
+          return container.innerHTML
+        })()
       }
     }
   })
